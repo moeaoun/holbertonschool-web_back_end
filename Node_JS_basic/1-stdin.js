33dfdffc2d46642a3f932465476134320e388125
root <moe_aoun@hotmail.com>
2025-05-19 10:57:30 +0000

    a

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
-console.log('Welcome to Holberton School, what is your name?');
-// await input
-process.stdin.resume();
-// once input is entered
+// Gets input from user and writes it to stdout
+process.stdin.setEncoding('utf-8'); // set encoding to utf-8
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
-  // check if input is coming from the terminal
-  if (process.stdin.isTTY) {
-    process.exit();
-  } else {
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  }
+  const name = process.stdin.read(); // read input from stdin
+  if (name) process.stdout.write(`Your name is: ${name}`);
+});
+/**
+ * If stdout process in stream comes from the terminal, end the process
+ * with a message
+ */
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });
